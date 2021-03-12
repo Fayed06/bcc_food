96f9852c08324db30755c05ea6557ea2bb5b80d6
XFAYZ\ASUS <alfayed06@student.ub.ac.id>
2021-03-12 23:19:27 +0800

    removing unused lines

--- Routes/restocat.routes.js
+++ Routes/restocat.routes.js
@@ -2,19 +2,10 @@ const express = require('express')
 const router = express.Router()
 const restocatController = require('../controllers/restocat.controller')
 
-
-
-
 // get all Categories
 router.get('/all', restocatController.findAll)
 
 // get one Categories
 router.get('/:id', restocatController.findOne)
-
-
-
-
-
-
 
 module.exports = router
